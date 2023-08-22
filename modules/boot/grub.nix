7d85c3e63829440b625d2f2a21a819801d38455a
son-of-satan <mtataryn555@gmail.com>
2023-08-22 14:25:11 +0300

    grub: set theme to "distro-grub-themes"

--- modules/boot/grub.nix
+++ modules/boot/grub.nix
@@ -7,6 +7,17 @@
       efiInstallAsRemovable = true;
       device = "nodev";
       gfxmode = "auto";
+      theme = pkgs.stdenv.mkDerivation {
+        pname = "distro-grub-themes";
+        version = "3.1";
+        src = pkgs.fetchFromGitHub {
+          owner = "AdisonCavani";
+          repo = "distro-grub-themes";
+          rev = "v3.1";
+          hash = "sha256-ZcoGbbOMDDwjLhsvs77C7G7vINQnprdfI37a9ccrmPs=";
+        };
+        installPhase = "cp -r customize/nixos $out";
+      };
     };
   };
 }
