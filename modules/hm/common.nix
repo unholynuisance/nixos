e98b651c2c34d484bb8a5ba0ab9dcf2e545d2b40
unholynuisance <mtataryn555@gmail.com>
2023-10-31 11:15:42 +0200

    hm/common: add texlive to home.packages

--- modules/hm/common.nix
+++ modules/hm/common.nix
@@ -17,6 +17,6 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    home.packages = with pkgs; [firefox fd ripgrep gnumake cmake gcc libtool perl telegram-desktop];
+    home.packages = with pkgs; [firefox fd ripgrep gnumake cmake gcc libtool perl telegram-desktop texlive.combined.scheme-full];
   };
 }
