f2715e98b4e5bd59ed7538bce84af30ea00edaad
unholynuisance <mtataryn555@gmail.com>
2023-11-17 19:43:08 +0200

    lib: add a type for UUIDs

--- lib/default.nix
+++ lib/default.nix
@@ -3,5 +3,9 @@
 in {
   forAllSystems = lib.genAttrs lib.systems.flakeExposed;
 
+  types = {
+    uuid = lib.types.strMatching "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}";
+  };
+
   storage = import ./storage.nix {inherit lib;};
 }
