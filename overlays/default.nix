fcf7701dcc82a07efcdbb61705bdda9aa73d993b
unholynuisance <mtataryn555@gmail.com>
2025-01-18 19:52:17 +0200

    overlays: remove unused code

--- overlays/default.nix
+++ overlays/default.nix
@@ -10,40 +10,10 @@
         master = inputs.nixpkgs-master.legacyPackages.${prev.system};
       };
 
-      # electron = final: prev: { # #
-      #   inherit (prev.master) electron electron_28;
-      # };
-
-      # teams = final: prev: { # #
-      #   inherit (prev.master) teams-for-linux;
-      # };
-
       xkeyboard_config_patched = (final: prev: {
         xkeyboard_config_patched = final.xkeyboard_config.overrideAttrs
           (old: { src = inputs.xkeyboard-config-src; });
       });
-
-      # tree-sitter = final: prev: prev.tree-sitter.override { abiVersion = 13; };
-
-      # tree-sitter-grammars = final: prev: {
-      #   tree-sitter-grammars = prev.tree-sitter-grammars // {
-      #     tree-sitter-rust =
-      #       prev.tree-sitter-grammars.tree-sitter-rust.overrideAttrs (_: {
-      #         nativeBuildInputs = [ final.nodejs final.tree-sitter ];
-      #         configurePhase = ''
-      #           tree-sitter generate --abi 13 src/grammar.json
-      #         '';
-      #       });
-
-      #     tree-sitter-cpp =
-      #       prev.tree-sitter-grammars.tree-sitter-cpp.overrideAttrs (_: {
-      #         nativeBuildInputs = [ final.nodejs final.tree-sitter ];
-      #         configurePhase = ''
-      #           tree-sitter generate --abi 13 src/grammar.json
-      #         '';
-      #       });
-      #   };
-      # };
     };
   };
 }
